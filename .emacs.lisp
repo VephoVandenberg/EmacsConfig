bf31a204528e4afbf9ecb765d18418e19a284f9f
Azamat Tskhovrebov <AzamatTheBest.cs.py@gmail.com>
2020-10-26 19:49:45 +0300

    Update .emacs.lisp

--- .emacs.lisp
+++ .emacs.lisp
@@ -1,4 +1,3 @@
-
 (setq c-default-style "bsd"
   c-basic-offset 4)
 
@@ -13,14 +12,20 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(custom-enabled-themes '(manoj-dark)))
+ '(custom-enabled-themes '(manoj-dark))
+ '(face-font-family-alternatives
+   '(("Monospace" "courier" "fixed")
+     ("Monospace Serif" "Courier 10 Pitch" "Consolas" "Courier Std" "FreeMono" "Nimbus Mono L" "courier" "fixed")
+     ("courier" "CMU Typewriter Text" "fixed")
+     ("Sans Serif" "helv" "helvetica" "arial" "fixed")
+     ("helv" "helvetica" "arial" "fixed"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cursor ((t (:background "green"))))
- '(font-lock-builtin-face ((t (:foreground "navajo white"))))
+ '(font-lock-builtin-face ((t (:foreground "lemon chiffon"))))
  '(font-lock-comment-delimiter-face ((t (:foreground "gray40"))))
  '(font-lock-comment-face ((t (:foreground "gray40" :slant oblique))))
  '(font-lock-constant-face ((t (:foreground "lemon chiffon" :weight bold))))
@@ -31,3 +36,8 @@
  '(font-lock-string-face ((t (:foreground "green"))))
  '(font-lock-type-face ((t (:foreground "lemon chiffon"))))
  '(font-lock-variable-name-face ((t (:foreground "lemon chiffon")))))
+
+(put 'upcase-region 'disabled nil)
+
+
+(set-frame-font "liberation mono")
